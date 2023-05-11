2d14298d6ae9412c1b8ec0f1e5047faa30b439b4
wac <wabaham9@gmail.com>
2023-05-11 20:45:07 +0000

    task 0 correction 3

--- 0x03-log_parsing/0-stats2.py
+++ 0x03-log_parsing/0-stats2.py
@@ -12,14 +12,14 @@ scodes = [200, 301, 400, 403, 404, 405, 500]
 code_cnt = {200: 0, 301: 0, 400: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 for line in stdin:
     code_no = line.strip().split(' ')
-    if int(code_no[-2]) in scodes:
-        code_cnt[int(code_no[-2])] += 1
-    tfile_size += int(code_no[-1])
-    counter += 1
-
     if counter == 10:
         counter = 0
         print('File size: {}'.format(tfile_size))
         for key, val in code_cnt.items():
             if val != 0:
                 print('{}: {}'.format(key, val))
+    else:
+        if int(code_no[-2]) in scodes:
+            code_cnt[int(code_no[-2])] += 1
+        tfile_size += int(code_no[-1])
+        counter += 1
