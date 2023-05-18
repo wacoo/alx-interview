69c70b788f03955e8a2fcb14571d1ed3205bb41f
wac <wabaham9@gmail.com>
2023-05-18 11:29:53 +0300

    task 0 correction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,7 +14,17 @@ the 8 least significant bits of each integer
 
 def validUTF8(data):
     ''' return True or False depending on whether the data given is UTF-8 '''
-    for val in data:
-        if val > 128:
-            return False
+    for val in range(len(data)):
+        if data[0] >= 192 and data[0] <= 247:
+            if data[0] >= 192 and data[0] <= 223:
+                if data[1] < 128 or data[1] > 191:
+                    return False
+            elif data[0] >= 224 and data[0] <= 239:
+                if (((data[1] and data[2]) < 128) or
+                        ((data[1] and data[2]) > 191)):
+                    return False
+            elif data[0] >= 240 and data[0] <= 247:
+                if (((data[1] and data[2] and data[3]) < 128) or
+                        ((data[1] and data[2] and data[3]) > 191)):
+                    return False
     return True
