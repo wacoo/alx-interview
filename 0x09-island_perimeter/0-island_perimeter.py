30db2c3bf444768c0e67c54a9d47f699b27967da
wac <wabaham9@gmail.com>
2023-06-22 21:06:07 +0300

    task 0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' return  the perimeter of the island in the grid '''
+
+
+def island_perimeter(grid):
+    ''' return perimeter '''
+    perim = 0
+    start = True
+    end = True
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                count = 0
+                if grid[i - 1][j] == 0:
+                    count += 1
+                if grid[i][j + 1] == 0:
+                    count += 1
+                if grid[i + 1][j] == 0:
+                    count += 1
+                if grid[i][j - 1] == 0:
+                    count += 1
+                perim += count
+    return perim
